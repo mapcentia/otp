815e9cb0304b8507754f8ad0f6b929d6a5af6582
Martin Høgh <mh@mapcentia.com>
2022-02-04 10:01:42 +0100

    OTP

--- browser/index.js
+++ browser/index.js
@@ -53,7 +53,7 @@ module.exports = module.exports = {
                 super(props);
 
                 this.state = {
-                    date: dayjs("2021-09-10").format("YYYY-MM-DD"),
+                    date: dayjs().format("YYYY-MM-DD"),
                     time: dayjs().format("HH:mm"),
                     numOfClass: 3,
                     startTime: 10,
@@ -61,11 +61,14 @@ module.exports = module.exports = {
                     intervals: [600, 1200, 1800],
                     startColor: '#ff0000',
                     endColor: '#00ff00',
-                    colorGradient: ['#aa5500', '#55aa00', '#00ff00']
+                    colorGradient: ['#aa5500', '#55aa00', '#00ff00'],
+                    features: [],
+                    legendChecks: [true, true, true]
                 };
 
                 this.load = this.load.bind(this);
                 this.handleChange = this.handleChange.bind(this);
+                this.handleLegendCheck = this.handleLegendCheck.bind(this);
             }
 
             handleChange(event) {
@@ -98,6 +101,40 @@ module.exports = module.exports = {
                 }, 100)
             }
 
+            handleLegendCheck(event) {
+                const checked = event.target.checked;
+                const value = event.target.value;
+                this.setState({
+                    legendChecks: this.state.legendChecks.map((e, i) => {
+                        let res = e
+                        if (i === parseInt((event.target.name))) {
+                            res = checked === true;
+                        }
+                        return res
+                    })
+                })
+                store.layer.eachLayer((l) => {
+                    if (parseInt(value) === l.feature.properties.time) {
+                        if (checked) {
+                            l.setStyle(
+                                {
+                                    fillOpacity: 1,
+                                    opacity: 1
+                                }
+                            )
+                        } else {
+                            l.setStyle(
+                                {
+                                    fillOpacity: 0,
+                                    opacity: 0
+                                }
+                            )
+                        }
+                        console.log(l)
+                    }
+                })
+            }
+
             createClasses() {
                 let start = this.state.startTime * 60;
                 let end = this.state.endTime * 60;
@@ -122,6 +159,7 @@ module.exports = module.exports = {
                 }
                 this.setState({intervals: arr})
                 this.setState({colorGradient: colorGradient.getArray()})
+                this.setState({legendChecks: Array(num).fill(true)})
             }
 
             componentDidMount() {
@@ -155,18 +193,23 @@ module.exports = module.exports = {
                         let time = feature.properties.time;
                         let i = this.state.intervals.indexOf(time);
                         let color = this.state.colorGradient[i];
+                        let opacity = this.state.legendChecks[i] ? 1 : 0;
                         return {
                             weight: 1,
                             color: color,
                             dashArray: '',
-                            fillOpacity: 1,
-                            opacity: 1
+                            fillOpacity: opacity,
+                            opacity: opacity
                         }
                     },
                     error: function () {
                     },
-                    onLoad: function () {
+                    onLoad: function (e) {
                         // cloud.get().zoomToExtentOfgeoJsonStore(this, 17)
+                        const features = e.geoJSON.features
+                        console.log(features)
+                        me.setState({features: features})
+
                     }
                 });
                 cloud.get().addGeoJsonStore(store);
@@ -174,7 +217,6 @@ module.exports = module.exports = {
                 search.init(function () {
                     me.makeSearch(this.geoJSON.features[0].geometry, true)
                 }, "opt-custom-search");
-
 
                 // Handle click events on map
                 // ==========================
@@ -190,22 +232,16 @@ module.exports = module.exports = {
                         if (me.state.active === false) {
                             return;
                         }
-
                         clicktimer = setTimeout(function (e) {
-
                             clicktimer = undefined;
-
                             var coords = event.getCoordinate(), p;
                             p = utils.transform("EPSG:3857", "EPSG:4326", coords);
-
                             me.makeSearch(
                                 {
                                     coordinates: [p.x, p.y],
                                     type: "Point"
                                 }
                             );
-
-
                         }, 250);
                     }
                 });
@@ -228,6 +264,19 @@ module.exports = module.exports = {
             }
 
             render() {
+                const legendItems = this.state.colorGradient.map((f, i) => <li key={i}><input type="checkbox" name={i}
+                                                                                              value={this.state.intervals[i]}
+                                                                                              checked={this.state.legendChecks[i]}
+                                                                                              onChange={this.handleLegendCheck}/>
+                    <div
+                        style={{
+                            width: "30px",
+                            height: "30px",
+                            backgroundColor: f,
+                            display: "inline-block"
+                        }}>&nbsp;</div>
+                    {this.state.intervals[i]}</li>);
+
                 return (
                     <div>
                         <div id="conflict-places" className="places" style={this.marginBottomXl}>
@@ -277,6 +326,9 @@ module.exports = module.exports = {
                                 <input type="color" id="otp-end-color" className="form-control"
                                        value={this.state.endColor}
                                        onChange={this.handleChange}/>
+                            </div>
+                            <div>
+                                <ul style={{listStyleType: "none", padding: 0, margin: 0}}>{legendItems}</ul>
                             </div>
                         </form>
                     </div>
